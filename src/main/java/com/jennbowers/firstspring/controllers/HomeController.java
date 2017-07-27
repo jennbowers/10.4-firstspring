2fe17bfdbe23dc936a736ec18040eab4392390bc
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-27 13:46:14 -0400

    Added form inputs and annotations

--- src/main/java/com/jennbowers/firstspring/controllers/HomeController.java
+++ src/main/java/com/jennbowers/firstspring/controllers/HomeController.java
@@ -1,13 +1,22 @@
 package com.jennbowers.firstspring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
+//    model is like a hash map
+//    each param is pulling from the browser, value corresponds with name
     @RequestMapping("/")
-    public String index() {
+    public String index(@RequestParam(value = "data", required = false) String data,
+                        @RequestParam(value = "lastname", required = false) String lastname,
+                        ModelMap model) {
+        System.out.println(data);
+        System.out.println(lastname);
+        model.addAttribute("pageData", data + " " + lastname);
         return "index";
     }
 }
