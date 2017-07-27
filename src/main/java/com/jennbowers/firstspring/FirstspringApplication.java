719e662c6580bd8661da2fea9e464d3145aedab0
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-27 11:02:01 -0400

    Added controller and template

--- src/main/java/com/jennbowers/firstspring/FirstspringApplication.java
+++ src/main/java/com/jennbowers/firstspring/FirstspringApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstspringApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(FirstspringApplication.class, args);
 	}
 }
